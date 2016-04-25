aadbba90510f0a676e9b825a675337beb8d9142b
Supachot Suriyachai (Admin) <adm-supachots@ad.reallyenglish.com>
2016-04-25 14:42:04 +0900

    Initial commit

---
+++ ps-scripts/client-djoin.ps1
@@ -0,0 +1,18 @@
+#
+# Script: client-djoin.ps1
+# Joins clients/servers to a domain
+#
+
+param($u,$p)
+
+$domainname = "reallyenglish"
+$u = "$domainname\$u"
+
+$securePassword = ConvertTo-SecureString -String $p -AsPlainText -Force
+$psCreds = new-object -typename System.Management.Automation.PSCredential -argumentlist $u, $securePassword
+
+$domainCheck = (Get-WmiObject -Class win32_computersystem).Domain
+if (!($domainCheck -eq $domainname)) {
+  Add-Computer -DomainName $domainname -Credential $psCreds -Force
+  eventcreate /t INFORMATION /ID 1 /L APPLICATION /SO "Ansible-Playbook" /D "joindomain-win: Added to the domain by ansible playbook."
+}
